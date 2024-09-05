c8ae62604ab39fff785e68c16a26ffe7589f1dda
Nguyen Duy <duynhu586@gmail.com>
2024-09-05 22:10:19 +0700

    prac 4 5 again

--- USTHweather/app/src/main/java/vn/edu/usth/usthweather/ForeCastFragment.java
+++ USTHweather/app/src/main/java/vn/edu/usth/usthweather/ForeCastFragment.java
@@ -4,16 +4,20 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ForeCastFragment#newInstance} factory method to
+ * Use the {@link ForecastFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ForeCastFragment extends Fragment {
+public class ForecastFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +28,7 @@ public class ForeCastFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ForeCastFragment() {
+    public ForecastFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +38,11 @@ public class ForeCastFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ForeCastFragment.
+     * @return A new instance of fragment ForecastFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ForeCastFragment newInstance(String param1, String param2) {
-        ForeCastFragment fragment = new ForeCastFragment();
+    public static ForecastFragment newInstance(String param1, String param2) {
+        ForecastFragment fragment = new ForecastFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,7 +63,20 @@ public class ForeCastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.activity_weather,)
-        return inflater.inflate(R.layout.fragment_fore_cast, container, false);
+        View v = inflater.inflate(R.layout.fragment_forecast, container, false);
+//        v.setBackgroundColor(0x20FF0000);
+//        LinearLayout linearLayout = new LinearLayout(getContext());
+//        linearLayout.setOrientation(linearLayout.VERTICAL);
+//        TextView textView = new TextView(getContext());
+//        textView.setText("thursday");
+//
+//        ImageView imageView = new ImageView(getContext());
+//        imageView.setImageResource(R.drawable.img);
+//
+//        linearLayout.addView(textView);
+//        linearLayout.addView(imageView);
+
+//        return linearLayout;
+        return v;
     }
 }
--- USTHweather/app/src/main/java/vn/edu/usth/usthweather/weatherActivity.java
+++ USTHweather/app/src/main/java/vn/edu/usth/usthweather/weatherActivity.java
@@ -1,6 +1,7 @@
 package vn.edu.usth.usthweather;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,53 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class weatherActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
+        // Create a new Fragment to be placed in the activity layout
+        ForecastFragment firstFragment = new ForecastFragment();
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.main, firstFragment).commit();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("onstart","onstart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("onPause","onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("onResume","onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("onStop","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("onDestroy","onDestroy");
     }
 }
