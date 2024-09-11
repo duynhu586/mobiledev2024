6f6ada749ed709c874978e1527f9b5949924045f
Nguyen Duy <duynhu586@gmail.com>
2024-09-11 14:48:53 +0700

    Practical work 7

--- USTHweather/app/src/main/java/vn/edu/usth/usthweather/weatherActivity.java
+++ USTHweather/app/src/main/java/vn/edu/usth/usthweather/weatherActivity.java
@@ -2,16 +2,18 @@ package vn.edu.usth.usthweather;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
 
 public class WeatherActivity extends AppCompatActivity {
+    private ViewpagerAdapter viewpagerAdapter;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +29,18 @@ public class WeatherActivity extends AppCompatActivity {
 //        WeatherFragment secondFragment = new WeatherFragment();
 //        getSupportFragmentManager().beginTransaction().add(
 //                R.id.main, secondFragment).commit();
+
 //
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-}
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+
+        viewPager = findViewById(R.id.Viewpager);
+        viewpagerAdapter = new ViewpagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewpagerAdapter);
+        };
+
 
     @Override
     protected void onStart() {
